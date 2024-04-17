3e9df3c5d37030b283e433f144a9d8c6f616651d
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-04-17 20:11:29 +0200

    reports

--- src/clinicaltrialsPOJO/Reports.java
+++ src/clinicaltrialsPOJO/Reports.java
@@ -1,5 +1,66 @@
 package clinicaltrialsPOJO;
 
+import java.util.Objects;
+
 public class Reports {
+	private String medicalHistory;
+	private String treatment;
+	
+	
+	public Reports(String medicalHistory, String treatment) {
+		super();
+		this.medicalHistory = medicalHistory;
+		this.treatment = treatment;
+	}
+
+
+	public String getMedicalHistory() {
+		return medicalHistory;
+	}
+
+
+	public void setMedicalHistory(String medicalHistory) {
+		this.medicalHistory = medicalHistory;
+	}
+
+
+	public String getTreatment() {
+		return treatment;
+	}
+
+
+	public void setTreatment(String treatment) {
+		this.treatment = treatment;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(medicalHistory, treatment);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reports other = (Reports) obj;
+		return Objects.equals(medicalHistory, other.medicalHistory) && Objects.equals(treatment, other.treatment);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reports [medicalHistory=" + medicalHistory + ", treatment=" + treatment + "]";
+	}
+	
+	
+	
+	
+	
 
 }
