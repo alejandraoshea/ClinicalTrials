ff4b648aa9b3e71670d11b79488bd6296ddc3042
nerealeria <nerealeria@localhost>
2024-04-17 20:13:16 +0200

    d

--- src/clinicaltrialsPOJO/InvestigationalProduct.java
+++ src/clinicaltrialsPOJO/InvestigationalProduct.java
@@ -1,5 +1,29 @@
 package clinicaltrialsPOJO;
 
 public class InvestigationalProduct {
+	
+	private Integer amount; 
+	private String description; 
+	private String type;
+	
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	} 
+	
 
 }
