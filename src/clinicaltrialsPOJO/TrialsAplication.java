16cb074436af9a9804118dc67ba4bcb7cecfeb14
PaulaMiron <paula.mirongracia@usp.ceu.es>
2024-04-17 20:18:51 +0200

    trialsAplication

---
+++ src/clinicaltrialsPOJO/TrialsAplication.java
@@ -0,0 +1,31 @@
+package clinicaltrialsPOJO;
+
+public class TrialsAplication {
+	private Date dateOfResolution;
+	private Date dateOfApplication;
+	private String resolution;
+	
+	
+	public Date getDateOfResolution() {
+		return dateOfResolution;
+	}
+	public void setDateOfResolution(Date dateOfResolution) {
+		this.dateOfResolution = dateOfResolution;
+	}
+	public Date getDateOfApplication() {
+		return dateOfApplication;
+	}
+	public void setDateOfApplication(Date dateOfApplication) {
+		this.dateOfApplication = dateOfApplication;
+	}
+	public String getResolution() {
+		return resolution;
+	}
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+	
+	
+	
+
+}
