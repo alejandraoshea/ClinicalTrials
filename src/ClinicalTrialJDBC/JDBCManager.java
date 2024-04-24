4a0184027216b182341f9ef96112aac6e6e2449a
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-04-24 19:42:58 +0200

    jdbc manager

--- src/ClinicalTrialJDBC/JDBCManager.java
+++ src/ClinicalTrialJDBC/JDBCManager.java
@@ -1,5 +1,21 @@
 package ClinicalTrialJDBC;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class JDBCManager {
+private Connection c = null;
+	
+	public JDBCManager() {
+		
+		try {
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e) {
+			System.out.print("Libraries not loaded");
+		}
 
 }
