c5d5e4b45ddafad0028c7512039edbf20bfaa39a
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-04-26 18:54:42 +0200

    lib

--- src/ClinicalTrialJDBC/JDBCManager.java
+++ src/ClinicalTrialJDBC/JDBCManager.java
@@ -1,31 +1,47 @@
 package ClinicalTrialJDBC;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCManager {
 private Connection c = null;
-	
-	public JDBCManager() {
-		
-		try {
-			
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
-		catch(ClassNotFoundException e) {
-			System.out.print("Libraries not loaded");
-		}
+public JDBCManager() {
 
-		private void createTables() {
-			try {
-				Statement stmt = c.createStatement();
-			}catch() {
-				
-			}
-		}
+	/*try {
 		
-				
+		
+	}
+	catch(SQLException e) {
+		e.printStackTrace();
+	}
+	catch(ClassNotFoundException e) {
+		System.out.print("Libraries not loaded");
+	}
+}
+*/
+	private void createTables() {
+		/*try {
+			
+		}catch(SQLException e) {
+			if(!e.getMessage().contains("already exists")) {
+				e.printStackTrace();
+			}			
+		}*/
+	}
+
+	public Connection getConnection() {
+		return c;
+	}
+
+	public void disconnect(){
+		try {
+			c.close();
+		}
+		catch(SQLException e){ 
+			e.printStackTrace();
+		}		
+	}
+
 }
