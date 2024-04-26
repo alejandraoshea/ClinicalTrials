f1c320a1e93481763300ad9ae6a2a7409c6e7fb8
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-04-26 19:55:47 +0200

    menu

--- src/UI/Menu.java
+++ src/UI/Menu.java
@@ -1,5 +1,62 @@
 package UI;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import ClinicalTrialInterfaces.AdministratorManager;
+import ClinicalTrialInterfaces.DoctorManager;
+import ClinicalTrialInterfaces.PatientManager;
+import ClinicalTrialInterfaces.SponsorManager;
+import ClinicalTrialInterfaces.EngineerManager;
+
+import ClinicalTrialJDBC.JDBCManager;
+import ClinicalTrialJDBC.JDBCAdministratorManager;
+import ClinicalTrialJDBC.JDBCDoctorManager;
+import ClinicalTrialJDBC.JDBCPatientManager;
+import ClinicalTrialJDBC.JDBCSponsorManager;
+import ClinicalTrialJDBC.JDBCEngineerManager;
+
+
 public class Menu {
+
+	private static JDBCManager jdbcmanager;
+	private static AdministratorManager adminmanager;
+	private static DoctorManager doctormanager;
+	private static PatientManager patientmanager;
+	private static SponsorManager sponsormanager;
+	private static EngineerManager engineermanager;
+	
+	private static BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+
+	public static void main(String[] args) {
+	
+		jdbcmanager = new JDBCManager();
+		adminmanager = new JDBCAdministratorManager(jdbcmanager); 
+		doctormanager = new JDBCDoctorManager(jdbcmanager);
+		patientmanager = new JDBCPatientManager(jdbcmanager);
+		sponsormanager = new JDBCSponsorManager(jdbcmanager);
+		engineermanager = new JDBCEngineerManager(jdbcmanager);
+		
+		try {
+			int choice;
+			do {
+				System.out.println("Choose an option");
+				System.out.println("0. Exit.");
+				
+				choice = Integer.parseInt(reader.readLine());
+								
+				switch(choice){
+				case 1: 
+					break;
+					
+				case 0:
+					jdbcmanager.disconnect();
+					System.exit(0);
+				}
+			}while(true);
+					
+		}catch(Exception e){
+			e.printStackTrace();}
+	}
 
 }
