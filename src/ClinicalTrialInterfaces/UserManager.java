6a2b2c6c5fa807d2d1b98f768e177a1532a6e46d
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-04-30 12:45:46 +0200

    user manager

--- src/ClinicalTrialInterfaces/UserManager.java
+++ src/ClinicalTrialInterfaces/UserManager.java
@@ -1,11 +1,11 @@
 package ClinicalTrialInterfaces;
 
 import java.util.List;
+
 import clinicaltrialsPOJO.Role;
 import clinicaltrialsPOJO.User;
 
-public class UserManager {
-	
+public interface UserManager {
 	public void connect();
 	public void disconnect();
 	public void newRole(Role r);
@@ -14,6 +14,4 @@ public class UserManager {
 	public Role getRole(Integer id);
 	public User getUser(String email);
 	public User checkPassword(String email, String pass);
-
-
 }
