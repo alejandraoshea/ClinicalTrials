c9c2794ebb14dd10c5e491626ea2dc6982e06883
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2024-05-08 19:59:58 +0200

    user

--- src/clinicaltrialsPOJO/User.java
+++ src/clinicaltrialsPOJO/User.java
@@ -13,8 +13,7 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
-
-import VetClinicPOJOs.Role;
+import clinicaltrialsPOJO.Role;
 
 
 @Entity
